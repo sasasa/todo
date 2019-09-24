5135daed912a249f0a036d3feaf6721d6afedfe5
sasasa <masaakisaeki@gmail.com>
2019-09-24 22:12:26 +0900

    heroku設定スクリプト修正

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-git remote add heroku https://git.heroku.com/<your-app-name>.git
+git remote add heroku https://git.heroku.com/sasasa-todo.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
-    login $HEROKU_LOGIN
+    login masaakisaeki@gmail.com
     password $HEROKU_API_KEY
 machine git.heroku.com
-    login $HEROKU_LOGIN
+    login masaakisaeki@gmail.com
     password $HEROKU_API_KEY
 EOF
 
